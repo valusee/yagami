ee6fa9c0addcb3cc5d0776acd527b33157c25843
valusee <valuse@proton.me>
2026-05-31 22:22:09 +0000

    loader

---
+++ loader.lua
@@ -0,0 +1,5 @@
+local placeid = game.PlaceId
+
+if placeid == 79393329652220 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/valusee/yagami/refs/heads/main/defusal/defusal.lua",true))()
+end
